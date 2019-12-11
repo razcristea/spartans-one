42410a37f1a8b3c6a7b17eb345c9af3c9919e829
Razvan Cristea <razcristea@gmail.com>
2019-12-12 00:21:09 +0200

    build deployment

---
+++ docs/precache-manifest.b4f7ba1659c2c0b935f33f2a5cc7a518.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0220a396714f2a1dbf233cbfe270a7",
+    "url": "/spartans-one/index.html"
+  },
+  {
+    "revision": "c923351a05c9d5127c55",
+    "url": "/spartans-one/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "182f2f67b032820a10eb",
+    "url": "/spartans-one/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c923351a05c9d5127c55",
+    "url": "/spartans-one/static/js/2.f70243c6.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/spartans-one/static/js/2.f70243c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "182f2f67b032820a10eb",
+    "url": "/spartans-one/static/js/main.ea826dc6.chunk.js"
+  },
+  {
+    "revision": "2ec097ca5f3344365ec4",
+    "url": "/spartans-one/static/js/runtime-main.afdf9105.js"
+  }
+]);
