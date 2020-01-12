f3863711531ab7009ba87e58187e568518d24e33
Mariana Frentsos <marianafrentsos@yahoo.com>
2020-01-12 15:18:32 +0200

    some changes edit modal

--- src/components/Route/Athletes/AthleteDetails/EditInfoModal.jsx
+++ src/components/Route/Athletes/AthleteDetails/EditInfoModal.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { MDBInput } from "mdbreact";
 
 import {
   MDBContainer,
@@ -7,7 +6,9 @@ import {
   MDBModal,
   MDBModalBody,
   MDBModalHeader,
-  MDBModalFooter
+  MDBModalFooter,
+  MDBInput,
+  MDBIcon
 } from "mdbreact";
 
 export default class EditInfoModal extends Component {
@@ -18,26 +19,43 @@ export default class EditInfoModal extends Component {
           isOpen={this.props.isShowing}
           toggle={() => this.props.show(!this.props.isShowing)}
         >
-          <MDBModalHeader toggle={() => this.props.show(!this.props.isShowing)}>
-            Edit athlete
+          <MDBModalHeader
+            toggle={() => this.props.show(!this.props.isShowing)}
+            className="m-4"
+          >
+            Edit Athlete
           </MDBModalHeader>
-          <MDBModalBody>
+
+          <MDBModalBody className="editModal">
             <div className="form-group">
               <MDBInput label="Name" icon="user" />
               <MDBInput label="Phone" icon="phone" />
               <MDBInput label="Email" icon="envelope-open" />
               <MDBInput label="Sex" icon="transgender" />
               <MDBInput label="Age" icon="baby" />
+              <MDBIcon icon="camera" size="lg" className="mr-3" />
+              Photo
+              <div className="custom-file m-2">
+                <input type="file" className="custom-file-input" />
+                <label
+                  className="custom-file-label"
+                  id="editPhoto"
+                  htmlFor="editPhoto"
+                ></label>
+              </div>
             </div>
           </MDBModalBody>
           <MDBModalFooter>
             <MDBBtn
-              color="secondary"
+              color="danger"
               onClick={() => this.props.show(!this.props.isShowing)}
             >
-              Close
+              <MDBIcon icon="ban" size="sm" className="m-2" /> Cancel
             </MDBBtn>
-            <MDBBtn color="primary">Save changes</MDBBtn>
+            <MDBBtn color="success">
+              <MDBIcon icon="check" size="sm" className="m-2" />
+              Save
+            </MDBBtn>
           </MDBModalFooter>
         </MDBModal>
       </MDBContainer>
